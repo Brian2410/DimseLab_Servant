ffc5341ed32f5f0a5b3fd5487f4e44d767df16cd
BGJ2410 <brian2410@outlook.dk>
2024-06-10 13:19:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def inventory():
     return redirect('https://brian2410.github.io/DimseLab_Servant/inventory.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)
